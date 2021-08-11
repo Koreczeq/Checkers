1ef500e96612664a1ce05f82cf481de56205016d
Koreczek <P.Skorupka@stud.elka.pw.edu.pl>
2021-08-11 13:26:01 +0200

    first commit

---
+++ checkers.cpp
@@ -0,0 +1,12 @@
+﻿// checkers.cpp : Defines the entry point for the application.
+//
+
+#include "checkers.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
